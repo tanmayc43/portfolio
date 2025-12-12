a1d42ad941f44121da381365d6843a971eebb2a9
Tanmay Chaturvedi <168345467+tanmayc43@users.noreply.github.com>
2025-12-12 06:58:10 +0530

    Updated path

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/portfolio/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
